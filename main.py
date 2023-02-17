3fe9d8945647d81abb17ee441dd955a33219c57f
itKillerWhale <br3027645@gmail.com>
2023-02-17 20:58:52 +0300

    Начало работы над сортировкой

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ import json
 from PyQt5 import uic, QtCore
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QFont
-from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QPushButton, QGroupBox, QVBoxLayout, QCheckBox
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QPushButton, QGroupBox, QVBoxLayout, QCheckBox, QWidget, \
+    QGridLayout
 
 
 class GroupBox(QGroupBox):
@@ -22,9 +23,6 @@ class GroupBox(QGroupBox):
         self.clicked.emit(self.title, child)
 
 
-
-
-
 class MyWidget(QMainWindow):
     def __init__(self):
         super().__init__()
@@ -32,9 +30,23 @@ class MyWidget(QMainWindow):
 
         self.vbox = QVBoxLayout()
 
+        self.update_tasks()
+
+    def update_tasks(self):
         with open('tasks.json') as file:
             data = json.load(file)
         tsaks = data["tasks"]
+
+        # Сортировка
+
+
+        scrollLayout = QVBoxLayout()
+
+        scrollW = QWidget()
+        self.scrollArea.setWidget(scrollW)
+
+        scrollW.setLayout(scrollLayout)
+        scrollLayout.setAlignment(QtCore.Qt.AlignTop)
 
         k = 0
         for key in tsaks.keys():
@@ -44,7 +56,8 @@ class MyWidget(QMainWindow):
             Group.setObjectName(f'task {k}')
             Group.clicked.connect(self.onGroupClick)
             Group.setFixedHeight(100)
-            Layout = QVBoxLayout()
+            Group.setFixedWidth(330)
+            Layout = QGridLayout()
             Group.setLayout(Layout)
 
             Item1_text = tsaks[key][0] + "_" + " ".join(key.split(";"))
@@ -60,16 +73,15 @@ class MyWidget(QMainWindow):
             Item3.setFixedHeight(21)
             Item3.setFont(QFont("MS Shell Dlg 2", 8))
 
-            Item4 = QLabel(" ".join(key.split(";")), objectName="Item4")
+            Item4 = QLabel("asd", objectName="Item4")
             Item4.setFixedHeight(21)
             Item4.setFont(QFont("MS Shell Dlg 2", 8))
 
-            Layout.addWidget(Item1)
-            Layout.addWidget(Item2)
-            Layout.addWidget(Item3)
-            self.vbox.addWidget(Group)
-
-        self.scrollArea.setLayout(self.vbox)
+            Layout.addWidget(Item1, 0, 0)
+            Layout.addWidget(Item2, 1, 0)
+            Layout.addWidget(Item3, 2, 0)
+            Layout.addWidget(Item4, 2, 1)
+            scrollLayout.addWidget(Group)
 
     def onGroupClick(self, title, obj):
         print(f"Group: {title}; objectName=`{obj.objectName()}`")
@@ -80,6 +92,5 @@ if __name__ == '__main__':
     ex = MyWidget()
     ex.show()
     sys.exit(app.exec_())
-
 
 # 36
